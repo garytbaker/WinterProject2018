f71f57caa47b2ad9e0ba99fef135961a023d32e0
isaiahsherfick <isaiahsherfick8@gmail.com>
2019-01-01 14:34:49 -0500

    completed basic functionality

---
+++ Findin' Deh Wai/Assets/Scripts/Bounce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+
+	public float jumpForce = 10f;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+		if (collision.relativeVelocity.y <= 0f)
+		{
+			if (rb != null)
+			{
+				Vector2 velocity = rb.velocity;
+				velocity.y = jumpForce;
+				rb.velocity = velocity;
+			}
+		}
+	}
+}
